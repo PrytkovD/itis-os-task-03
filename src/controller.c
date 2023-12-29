92ff0b0541226b3e00a7ae9888fbf8d2e6a5c934
PrytkovD <prytkovd.03@gmail.com>
2023-12-29 09:28:54 +0000

    Fix

--- src/controller.c
+++ src/controller.c
@@ -5,6 +5,12 @@
 #include <fcntl.h>
 #include <string.h>
 
+int produced = 0;
+
+void signal_handler(int signal) {
+	printf("Produced: %d\n", produced);
+}
+
 int main(int argc, char *argv[]) {
 	int pipe10[2], pipe02[2], pipe20[2];
 
@@ -26,6 +32,11 @@ int main(int argc, char *argv[]) {
 
 	if (p1 == 0) {
 		// child process
+		close(pipe02[0]);
+		close(pipe02[1]);
+		close(pipe20[0]);
+		close(pipe20[1]);
+
 		close(pipe10[0]);
 		dup2(pipe10[1], STDOUT_FILENO);
 
@@ -48,6 +59,9 @@ int main(int argc, char *argv[]) {
 
 	if (p2 == 0) {
 		// child process
+		close(pipe10[0]);
+		close(pipe10[1]);
+
 		close(pipe02[1]);
 		close(pipe20[0]);
 		dup2(pipe02[0], STDIN_FILENO);
@@ -59,24 +73,15 @@ int main(int argc, char *argv[]) {
 		perror("execve");
 		_exit(EXIT_FAILURE);
 	} else {
+		// parent process
 		close(pipe02[0]);
 		close(pipe20[1]);
 	}
 
-//	close(pipe10[0]);
-//	close(pipe10[1]);
-//	close(pipe02[1]);
-//	close(pipe20[0]);
-
 	char buffer10[1024] = "";
 	char buffer20[1024] = "";
-	int count = 0;
 
-//	int result = fcntl(pipe20[0], F_SETFL, fcntl(pipe20[0], F_GETFL) | O_NONBLOCK);
-//
-//	if (result < 0) {
-//		perror("fcntl");
-//	}
+	signal(SIGUSR1, signal_handler);
 
 	while (read(pipe10[0], buffer10, sizeof(buffer10)) >= 0) {
 		write(pipe02[1], buffer10, strlen(buffer10));
@@ -86,11 +91,10 @@ int main(int argc, char *argv[]) {
 			buffer20[strlen(buffer20) - 1] = 0;
 		}
 		printf("%s = %s", buffer10, buffer20);
-		count++;
+		produced++;
 	}
 
-	signal(SIGUSR1, SIG_IGN);
-	printf("Produced: %d\n", count);
+	printf("Produced: %d\n", produced);
 
 	return 0;
 }
